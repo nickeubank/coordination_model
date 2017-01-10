e5d94ea97cb3fe4156f0eb0b7f10c8246e70d654
Nick Eubank <nickeubank@gmail.com>
2017-01-10 15:52:59 -0800

    add convergence criteria

--- coordination_model.py
+++ coordination_model.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import igraph as ig
 import numpy.random as npr
+import numpy as np
 import os
 import random
 import warnings
@@ -15,7 +16,10 @@ except:
     raise EnvironmentError("Not guaranteed to work with less than python 3.4")
 
 
-def run_coordination_simulation(graph, num_runs=1, num_steps=4, 
+def run_coordination_simulation(graph, num_runs=1, convergence=False,
+                                num_steps=None, convergence_threshold=None,
+                                convergence_period=None,
+                                convergence_max_steps=None,
                                 beta_mean=0.5, beta_std=0.1,
                                 debug=False, np_seed=None):
     """ 
@@ -25,7 +29,18 @@ def run_coordination_simulation(graph, num_runs=1, num_steps=4,
 
     graph: iGraph object on which to run model
     num_runs: number of times to run simulation
-    num_steps: number of steps to execute each run
+    convergence: True/False -- whether to use convergence 
+              threshold instead of finite number of steps. 
+              If True, must specify `convergence_threshold`.
+              If False, must specify `num_steps`.
+    num_steps: number of steps to execute each run for 
+               finite run. Only for use when 
+               `convergence=False`.
+    convergence_threshold: maximum share of nodes allowed to 
+                be changing step to step in converged state. 
+    convergence_period: number of steps must stay below
+                threshold to be considered converged. 
+    convergence_max_steps: limit to steps allowed if converging.  
     beta_mean: mean of normal distribution from which 
                initial preferences (beta) drawn.
     beta_std: std deviation of normal distribution from which 
@@ -34,7 +49,9 @@ def run_coordination_simulation(graph, num_runs=1, num_steps=4,
             step of simulation and will run test suite. If 
             `num_runs` more than one, will overwrite each run, 
             leaving only most recent run.
-    np_seed: passed to numpy.random.seed() before beta's drawn. 
+    np_seed: passed to numpy.random.seed() before beta's drawn.
+            Note the SAME SEED is used on every run -- this is mostly 
+            for testing. 
     
 
     Returns coordination scores between 0 & 1, where 1 means
@@ -51,37 +68,107 @@ def run_coordination_simulation(graph, num_runs=1, num_steps=4,
         print('running test suit')
         test_suite()
 
+    if not convergence:
+        if convergence_threshold is not None or convergence_period is not None or convergence_max_steps is not None:
+                raise ValueError("Can't use convergence arguments if convergence not true")
+
+    if convergence:
+        if num_steps is not None:
+            raise ValueError("cannot use num_steps with convergence")
+        if convergence_threshold is None or convergence_period is None or convergence_max_steps is None:
+            raise ValueError("If convergence is True, must specify all convergence arguments")
+
 
     # Start actual run. Create results storage vehicle and start 
     # running simulations!
 
-    results = pd.Series(index=range(num_runs))
+    results = pd.DataFrame(columns=['coordination', 'converged', 'steps'],
+                           index=range(num_runs))
 
     for run in range(num_runs):
         if debug:
             print('starting run {}'.format(run))
 
-        results[run] = single_simulation_run(graph, num_steps, beta_mean, 
-                                             beta_std, debug, np_seed)
+        output = single_simulation_run(graph, convergence=convergence,
+                                             num_steps=num_steps, 
+                                             convergence_threshold=convergence_threshold,
+                                             convergence_period=convergence_period,
+                                             convergence_max_steps=convergence_max_steps, 
+                                             beta_mean=beta_mean, 
+                                             beta_std=beta_std, 
+                                             debug=debug, 
+                                             np_seed=np_seed)
     
-    assert (results >= 0).all()
-    assert (results <= 1 ).all()
+        results.loc[run, 'coordination'] = output[0]
+        results.loc[run, 'converged'] = output[1]
+        results.loc[run, 'steps'] = output[2]
+
+
+    assert (results.coordination >= 0).all()
+    assert (results.coordination <= 1 ).all()
 
     return results
 
 
-def single_simulation_run(graph, num_steps, beta_mean, beta_std, debug, np_seed):
+def single_simulation_run(graph, convergence, num_steps, 
+                          convergence_threshold,
+                          convergence_period,
+                          convergence_max_steps, 
+                          beta_mean, beta_std, 
+                          debug, np_seed):
+
     model = Simulation_State(graph, beta_mean, beta_std, debug, np_seed)
 
-    for step in range(num_steps):
-        model.iterate(step)
 
+    # The model can run in two states -- 
+    # Fixed number of steps or convergence. 
+    if convergence is not True:
+        for step in range(num_steps):
+            model.iterate(step)
+
+        converged = False
+
+        # want number of steps in normal numbers, not 0-counting. 
+        step +=1 
+
+    if convergence is True:
+
+        continue_running = True
+        step = 0
+        periods_under_threshold = 0
+        converged = False
+
+        while continue_running:
+            prior_state = model.status['binary_pref'].copy()
+
+            model.iterate(step)
+
+            post_state = model.status['binary_pref'].copy()
+
+            changes = np.mean(post_state != prior_state)
+            assert ((changes >=0) & (changes <=1)).all()
+
+            step +=1
+
+            # Check state!
+            if changes < convergence_threshold:
+                periods_under_threshold +=1
+            
+            if periods_under_threshold >= convergence_period:
+                continue_running = False
+                converged = True
+
+            if step >= convergence_max_steps:
+                continue_running = False
+
+
+    # Now calculate share coordinated in final state. 
     share_for_1 = model.status.binary_pref.mean()
 
     # Correct so scaled 0 to 1
     coordination = abs(share_for_1 - 0.5) * 2
 
-    return coordination
+    return coordination, converged, step
 
 class Simulation_State(object):
     """ 
@@ -183,13 +270,15 @@ def test_suite():
 
     results = run_coordination_simulation(g, num_runs = 10, num_steps=5, 
                                           beta_mean=1000, beta_std=0.1) 
-    assert len(results) == 10
-    assert (results == 1).all()  
+    assert len(results.coordination) == 10
+    assert (results.coordination == 1).all()  
+    assert (results.converged == False).all()  
+    assert (results.steps == 5).all()  
 
     results = run_coordination_simulation(g, num_runs = 10, num_steps=5, 
                                           beta_mean=-1000, beta_std=0.1) 
-    assert len(results) == 10
-    assert (results == 1).all()  
+    assert len(results.coordination) == 10
+    assert (results.coordination == 1).all()  
 
 
     # Make sure deterministic aside from betas. 
@@ -197,7 +286,7 @@ def test_suite():
     results = run_coordination_simulation(g, num_runs = 10, num_steps=5, 
                                           beta_mean=0.5, beta_std=0.1,
                                           np_seed=5) 
-    assert (results == results[0]).all()
+    assert (results.coordination == results.coordination[0]).all()
 
     # Graph with two people should never converge if betas in 0-0.5 and 0.5-1. 
     
@@ -213,7 +302,71 @@ def test_suite():
     results = run_coordination_simulation(g, num_runs = 10, num_steps=5, 
                                           beta_mean=0.5, beta_std=0.2,
                                           np_seed=1) 
-    assert (results == 0).all()
+    assert (results.coordination == 0).all()
+
+
+    # Now run with convergence threshold. Should never 
+    # arrive and should hit it's max iterations. 
+    results = run_coordination_simulation(g, num_runs = 10, convergence=True,
+                                          convergence_threshold=0.2, 
+                                          convergence_period=3,
+                                          convergence_max_steps=10,
+                                          beta_mean=0.5, beta_std=0.2,
+                                          np_seed=1) 
+
+    assert (results.converged == False).all()
+    assert (results.steps == 10).all()
+
+    # Change threshold to 1, and should converge nicely (albeit trivially).   
+    results = run_coordination_simulation(g, num_runs = 10, convergence=True,
+                                          convergence_threshold=1.1, 
+                                          convergence_period=3,
+                                          convergence_max_steps=10,
+                                          beta_mean=0.5, beta_std=0.2,
+                                          np_seed=1) 
+
+    assert (results.converged == True).all()
+    assert (results.steps == 3).all()
+
+
+
+    # Run for immediate convergence
+    results = run_coordination_simulation(g, num_runs = 10, convergence=True,
+                                          convergence_threshold=0.2, 
+                                          convergence_period=3,
+                                          convergence_max_steps=10,
+                                          beta_mean=100, beta_std=0.1,
+                                          np_seed=2) 
+    assert (results.converged == True).all()
+    assert (results.steps == 3).all()
+
+    # Check threshold values. 
+    # Now has two vertices which will flip each time, and 8 that
+    # are disconnected and thus static. 
+    # So should converge at threshold of 0.21 (since 2 flipping = 0.2)
+    # but not at 0.1
+    g.add_vertices(range(2,10))
+    results = run_coordination_simulation(g, num_runs = 10, convergence=True,
+                                          convergence_threshold=0.21, 
+                                          convergence_period=3,
+                                          convergence_max_steps=10,
+                                          beta_mean=0.5, beta_std=0.2,
+                                          np_seed=1) 
+
+    assert (results.converged == True).all()
+    assert (results.steps == 3).all()
+
+
+    results = run_coordination_simulation(g, num_runs = 10, convergence=True,
+                                          convergence_threshold=0.1, 
+                                          convergence_period=3,
+                                          convergence_max_steps=10,
+                                          beta_mean=0.5, beta_std=0.2,
+                                          np_seed=1) 
+
+    assert (results.converged == False).all()
+    assert (results.steps == 10).all()
+
 
 
 
